98e724a129d8af25e42d257d17ca4655f2f81a03
dhanraj-12 <dj004786@gmail.com>
2025-06-26 00:49:19 +0530

    typescript

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express"
+import { Request, Response } from "express";
+const app = express();
+
+
+app.get("/",(req: Request, res: Response)=>{
+    res.send("Hello app is running");
+})
+
+app.listen(3000,()=>console.log("App is running on 3000"))
