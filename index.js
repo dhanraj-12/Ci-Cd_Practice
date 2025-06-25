0b66d5b6af1de8d598187a04339b691d1c4421a4
dhanraj-12 <dj004786@gmail.com>
2025-06-26 00:15:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get('/', (req, res) => res.send('Hello World! app1 is running'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
